5625d4c39dd820d78c9a4da3e977875ea6c325ff
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2025-02-26 13:06:40 +0100

    deit event overlay once only when start the app

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -2,12 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const overlay = document.getElementById("eventLandingOverlay");
   const closeButton = document.querySelector(".close-overlay");
 
+  // Check if the overlay was already shown
+  if (!localStorage.getItem("overlayShown")) {
+    overlay.style.display = "flex"; // Show the overlay
+
+    // Set 'overlayShown' in localStorage
+    localStorage.setItem("overlayShown", "true");
+
+    // Optional: Automatically hide the overlay after a specific time
+    setTimeout(() => {
+      overlay.style.display = "none";
+    }, 10000); // hides after 10 seconds
+  } else {
+    overlay.style.display = "none"; // Hide the overlay if already shown before
+  }
+
   closeButton.addEventListener("click", () => {
     overlay.style.display = "none";
   });
-
-  // Optional: Automatically hide the overlay after a few seconds
-  setTimeout(() => {
-    overlay.style.display = "none";
-  }, 100000); // hides after 10 seconds
 });
